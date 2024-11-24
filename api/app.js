86ec0a752ec78ec19c9fe73af3886e050e1a6cd9
pc <caglar.ylmz@outlook.com>
2024-11-25 00:14:48 +0300

    Dynamic Routing Yapısı Oluşturuldu

--- api/app.js
+++ api/app.js
@@ -7,9 +7,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
+//Routes Define
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,14 +16,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Routes Use
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var config = require("../config");
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', config: config });
-});
+// Dynamic Routes ==> //
+const fs = require("fs");
+
+let routes = fs.readdirSync(__dirname);
+
+for (let route of routes) {
+  if (route.includes(".js") && route != "index.js") {
+    router.use("/" + route.replace(".js", ""), require('./' + route));
+  }
+}
+// Dynamic Route //
+
 
 module.exports = router;
